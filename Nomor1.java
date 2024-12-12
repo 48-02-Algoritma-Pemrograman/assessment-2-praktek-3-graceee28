51077213c53f25e622e7907ff3d970b16ef68871
graceee28 <183887439+graceee28@users.noreply.github.com>
2024-12-12 14:31:17 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,7 +6,44 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        
+    public static double hitungLuasSegitiga(double alas, double tinggi) {
+        return 0.5 * alas * tinggi;
+    }
 
+    public static void printFizzBuzz(int N) {
+        for (int i = 0; i <= N; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nilai alas: ");
+        double alas = scanner.nextDouble();
+        System.out.println("Masukkan nilai tinggi: ");
+        double tinggi = scanner.nextDouble();
+        
+        double luas = hitungLuasSegitiga(alas, tinggi);
+        System.out.println("Luas segitiga adalah: " + luas);
+        
+        System.out.println("Masukkan nilai N: ");
+        int N = input.nextInt();
+        printFizzBuzz(N);
+
+    }
+    
+}
+    
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
